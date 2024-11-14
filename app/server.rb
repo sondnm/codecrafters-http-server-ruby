bf7cb96b3127a04b12f0d8c1307c8e852ee5f2bb
Son Dang <sondangdev@gmail.com>
2024-11-15 01:20:59 +0700

    Add handler for echo path

--- app/server.rb
+++ app/server.rb
@@ -7,10 +7,15 @@ server = TCPServer.new("localhost", 4221)
 loop do
   client = server.accept
 
-  request, _headers, _body = client.gets.split("\r\n")
+  request, *_headers, _body = client.gets.split("\r\n")
   _protocol, path, _version = request.split(" ")
 
-  if path == "/"
+  matched = path.match("/echo/(.+)")
+  if matched
+    str = matched[1]
+    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{str.size}\r\n\r\n#{str}"
+    client.puts response
+  elsif path == "/"
     client.puts "HTTP/1.1 200 OK\r\n\r\n"
   else
     client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
