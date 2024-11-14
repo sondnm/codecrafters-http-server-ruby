969df09d25db9519c1574331a9ed3ec3d6582c4a
Son Dang <sondangdev@gmail.com>
2024-11-15 01:55:42 +0700

    Add user-agent header

--- app/server.rb
+++ app/server.rb
@@ -7,15 +7,29 @@ server = TCPServer.new("localhost", 4221)
 loop do
   client = server.accept
 
-  request, *_headers, _body = client.gets.split("\r\n")
+  request = client.gets
   _protocol, path, _version = request.split(" ")
 
-  matched = path.match("/echo/(.+)")
-  if matched
-    str = matched[1]
+  case path
+  when /\/echo\/(.+)/
+    str = Regexp.last_match(1)
     response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{str.size}\r\n\r\n#{str}"
     client.puts response
-  elsif path == "/"
+  when "/user-agent"
+    header = ""
+    loop do
+      header = client.gets
+      header.match?(/^User-Agent: .*$/) ? break : header = ""
+    end
+
+    if header.empty?
+      client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+    else
+      _header_key, header_value = header.split(" ")
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{header_value.size}\r\n\r\n#{header_value}"
+      client.puts response
+    end
+  when "/"
     client.puts "HTTP/1.1 200 OK\r\n\r\n"
   else
     client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
